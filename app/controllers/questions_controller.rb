c486dc4e72ca9a987ef5ec033a71987b7dd2b8b6
Gustavo Sousa <gussousa@gmail.com>
2020-11-25 14:35:47 -0300

    Changes in responses.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,15 @@
 class QuestionsController < ApplicationController
-  def ask
-  end
-  
+  def ask; end
+
   def answer
     @question = params[:question] unless params[:question].nil?
     @answer =
-      if @question.downcase.include?('i am going to work') || @question.downcase.include?('i will work now')
-        'Great!'
-      elsif @question.include?('?')
+      if @question.include?('?')
         'Silly question, get dressed and go to work!'
-      elsif @question.length == 0
-        @question = "..."
+      elsif @question.downcase.include?('i am going to work') || @question.downcase.include?('Vou trabalhar')
+        'Great!'
+      elsif @question.length.zero?
+        @question = '...'
         'Yeah! You don\'t have to talk, you have to work! Now work!!'
       else
         'I don\'t care, get dressed and go to work!'
